c8b01547526d17a21cef08fee1907f39ddb07f14
albert <55817155+Albertcacv@users.noreply.github.com>
2022-07-17 17:57:47 +0200

    ⚒️ add components
    banner, footer, layout, navbar and properties

---
+++ components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/layout';
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+			© 2021 Roomy, Inc.
+		</Box>
+	);
+};
+
+export default Footer;
